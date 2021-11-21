3fd0db76a65287a730c9d55c6a30412664ea7268
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2021-11-21 11:46:06 +0100

    Update create_director_candidate_page.py
    Works with new MemberClicks system - updated for 2021 Elections

--- scripts/elections/create_director_candidate_page.py
+++ scripts/elections/create_director_candidate_page.py
@@ -5,7 +5,6 @@ Generate the HTML page for director candidates.
 The inputs this script requires are:
 - export of director candidate form
 - export of "active members"
-- export of "inactive members"
 
 Please ensure that the names of the exported files match the names set in this
 script below.
@@ -13,23 +12,22 @@ script below.
 
 import csv
 
-year = 2020
+year = 2021
 fn = "ReceiptExport.csv"
 active_members = "active.csv"
-inactive_members = "inactive.csv"
 output_file = "candidates_{}.html".format(year)
 template = """
-
+<br/>
 <table border="1" frame="above" id="{}">
     <tbody>
         <tr valign="middle"  style="background : #eff8fd">
-            <td style="border: 0px; width:100px"><img style="width: 100px; float: left; margin-right: 10px; margin-left: 10px;" src="{}" alt="Picture of {}" /></td>
-            <td style="border: 0px; width:200px; margin-top: 0px; margin-right: 10px; margin-bottom: 5px; margin-left: 10px; outline-width: 0px; outline-style: initial; outline-color: initial; line-height: 18px; padding: 0px;">
+            <td style="border: 0px; width:125px"><img style="width: 100px; float: left; margin-right: 10px; margin-left: 10px;" src="{}" alt="Picture of {}" /></td>
+            <td style="border: 0px; width:175px; margin-top: 0px; margin-right: 10px; margin-bottom: 5px; margin-left: 10px; padding-left: 10px; padding-top: 10px; outline-width: 0px; outline-style: initial; outline-color: initial; line-height: 18px;">
                 <p><strong><a href="{}" target="_blank">{}</a></strong></p>
                 <address>{}</address>
                 <address>{}<br />{}<br />{}</address>
             </td>
-            <td style="width:400px; border: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; outline-width: 0px; outline-style: initial; outline-color: initial; line-height: 18px; padding: 0px;">
+            <td style="width:400px; border: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; outline-width: 0px; outline-style: initial; outline-color: initial; line-height: 18px; padding: 10px;">
                 <p>{}</p>
             </td>
         </tr>
@@ -42,14 +40,14 @@ template = """
             <td style="border: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 1px; margin-left: 0px; outline-width: 0px; outline-style: initial; outline-color: initial; line-height: 18px; padding: 0px;">
                 <p><h2>Motivation:</h2>{}</p>
                 <p>{}</p>
-                <p>{}</p>
+                {}
             </td>
         </tr>
     </tbody>
 </table>
+<br/>
 """
 
-out = open(output_file, "w")
 body = ""
 candidates = {}
 
@@ -57,7 +55,7 @@ with open(fn, 'r') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
 
     for row in reader:
-        candidates["{} {}".format(row['Last Name'].title(), row['First Name'].title())] = row
+        candidates["{} {}".format(row['Name | Last'].title(), row['Name | First'].title())] = row
 
     ordered = []
     for n in list(candidates.keys()):
@@ -68,35 +66,22 @@ with open(active_members, 'r') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
 
     for row in reader:
-        name = "{} {}".format(row['Last Name'].title(), row['First Name'].title())
+        name = "{} {}".format(row['[Name | Last]'].title(), row['[Name | First]'].title())
         if name in candidates:
-            for key in ['Group', 'City', 'Institution', 'Country']:
+            for key in ['[Group]', '[Address | Preferred | City]', '[Organization]', '[Address | Preferred | Country]']:
                 candidates[name][key] = row[key].title()
-
-"""
-with open(inactive_members, 'r') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-
-    for row in reader:
-        name = "{} {}".format(
-            row['Last Name'].title(), row['First Name'].title())
-
-        if name in candidates:
-            for key in ['Group', 'City', 'Institution', 'Country']:
-                candidates[name][key] = row[key].title()
-"""
 
 for surname in ordered:
 
     row = candidates[surname]
-    name = row['First Name'] + " " + row['Last Name']
+    name = row['Name | First'] + " " + row['Name | Last']
 
     # SHOULD BE RETRIEVED FROM VALID MEMBERS
-    if 'Country' in row:
-        addr1 = row['Institution']
-        addr2 = row['City']
-        addr3 = row['Country']
-        memb = row['Group']
+    if '[Address | Preferred | Country]' in row:
+        addr1 = row['[Organization]']
+        addr2 = row['[Address | Preferred | City]']
+        addr3 = row['[Address | Preferred | Country]']
+        memb = row['[Group]']
     else:
         print(surname, "is not a valid member: details should be manually provided")
         addr1 = ''
@@ -107,24 +92,26 @@ for surname in ordered:
     info = row['Biography']
     mot = row['Motivation']
     oth = row['Other activities']
-    if len(oth) > 1:
-        oth = '<h2>Other information:</h2>{}'.format(oth)
+    if len(oth) > 1 and oth != "Please mention any other contributions you have made/are making to the computational neuroscience community (400 characters maximum, extra text will be removed).":
+        oth = '<p><h2>Other information:</h2>{}</p>'.format(oth)
+    else:
+        oth = ""
 
     att = row['Attend CNS']
     if att == 'none':
         att = '0'
 
-    rev = "Programme Committee/Local Organizing Committee member: {} meeting(s)".format(row['Member PC or LO'] if row['Member PC or LO'] != "never" else "0")
+    rev = "<strong>Programme Committee/Local Organizing Committee member:</strong> {} ".format("Yes (" + row['Member PC or LO'] + ")" if row['Member PC or LO'] != "never" else "No")
 
     member_year = row['Member start']
 
-    particip = "<h2>OCNS and CNS participation:</h2>Attended {} CNS meeting(s).  {}. OCNS member since {}.".format(att, rev, member_year)
+    particip = "<h2>OCNS and CNS participation:</h2><p><strong># of CNS meetings attended: </strong>{}</p><p>{}</p><p><strong>OCNS member since:</strong> {}</p>".format(att, rev, member_year)
 
-    pic = "https://ocns.memberclicks.net/assets/images/Elections/{}/{}.jpg".format(year, row['Last Name'])
+    pic = "https://ocns.memberclicks.net/assets/images/Elections/{}/{}.jpg".format(year, row['Name | Last'])
     url = row['URL']
 
     body += template.format(name, pic, name, url, name, memb, addr1, addr2, addr3,
                             info, mot, particip, oth)
 
-out.write(body)
-out.close()
+with open(output_file, "w") as outfile:
+    outfile.write(body)
